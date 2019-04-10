c67268b26d50e25c398cdcde743bea9a2e550136
ramanoid <ramanunnim1995@gmail.com>
2019-04-10 14:47:43 +0530

    eebgghe

--- server.js
+++ server.js
@@ -5,13 +5,27 @@ a.engine('handlebars',exphb({defaultLayout:'mainp'}));
 a.set('view engine','handlebars');
 a.use(express.static('views/static'));
 a.use(express.urlencoded());
-a.post('/',(req,res)=>{
+a.post('/login',(req,res)=>{
   var name=req.body.uname;
   var pwd=req.body.passwrd;
 //   var email=req.body.email;
-if(name=="admin" && pwd=="1234")
+  if(name=="admin" && pwd=="1234")
     res.send('login success');
-else
+  else
   res.send(name+ "is not yet a registered member" );
   
-}).listen(process.env.PORT || 3000);
+});
+a.get('/',(req,res)=>{
+  res.render('index');
+  //res.end();
+});
+a.get('/about',(req,res)=>{
+  res.render('about');
+})
+a.get('/home',(req,res)=>{
+  res.render('home');
+})
+a.get('/index',(req,res)=>{
+  res.render('indexo');
+})
+ .listen(process.env.PORT || 3000);
